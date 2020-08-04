810558a198558b481f9af67ae7e503131270ac82
monomadic <deathdisco@protonmail.com>
2020-08-05 03:35:40 +1000

    filesystem and structopt rust examples

--- rust/filesystem.rs
+++ rust/filesystem.rs
@@ -12,3 +12,15 @@ use walkdir::WalkDir;
 for entry in WalkDir::new("foo") {
     println!("{}", entry?.path().display());
 }
+
+//
+use std::fs::File;
+use std::io::prelude::*;
+
+pub(crate) fn readfile(pathbuf: &std::path::PathBuf) -> Result<String, std::io::Error> {
+    let mut f = File::open(pathbuf.clone())?;
+    let mut buffer = String::new();
+
+    f.read_to_string(&mut buffer)?;
+    Ok(buffer)
+}
---
+++ rust/structopt.rs
@@ -0,0 +1,37 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "astryx")]
+struct Opt {
+    /// Command
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// start a server
+    Serve {
+        /// Input file
+        #[structopt(parse(from_os_str))]
+        file: PathBuf,
+        port: Option<u32>,
+    },
+    /// build the project
+    Build {
+        /// Input file
+        #[structopt(parse(from_os_str))]
+        file: PathBuf,
+    },
+    New,
+}
+
+pub fn main() {
+    let opt = Opt::from_args();
+
+    match opt.command {
+        Command::Serve{ file, port } => server::start(file, port.unwrap_or(8888)),
+        Command::Build{ .. } => Ok(()),
+        Command::New => new_project(),
+    };
+}
