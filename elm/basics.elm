9b9a0776e9e7d31693791b86f6cf4c2ce6139d56
monomadic <deathdisco@protonmail.com>
2020-09-25 22:55:32 +1000

    elm

---
+++ elm/basics.elm
@@ -0,0 +1,28 @@
+-- hello world example
+
+module Main exposing (..)
+
+import Html
+
+main =
+   Html.text "Hello, World!"
+
+
+-- div and styles
+
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+astyledMessage =
+  div
+    [ style "background-color" "red"
+    , style "height" "90px"
+    , style "width" "100%"
+    ]
+    [ text "Hello!"
+    ]
+
+main =
+  astyledMessage
